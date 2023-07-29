2c4456d2a147513b7a176c162fac625c28df9c73
Alina <alinapestova@gmail.com>
2023-07-29 20:23:21 +0300

    в файле app_lesson_4\urls.py прописываем urlpatterns = [     path('lesson_4', index) ]

--- advertisements/app_adv/models.py
+++ advertisements/app_adv/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Advertisements(models.Model):
+    title = models.CharField('заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+
+    # для переименования прописываем код со странички https://django.fun/ru/docs/django-orm-cookbook/2.0/table_name/
+    # затем python manage.py makemigrations
+    # наконец, python manage.py migrate
+
+
+    def __str__(self):
+        return f'{__class__.__name__}(id={self.id}, title={self.title}, price={self.price})'
+
+    # https://stackoverflow.com/questions/45483417/what-is-doing-str-function-in-django
+    # from app_adv.models import Advertisements
+    # queryset = Advertisements.objects.all()
+    # queryset[0]

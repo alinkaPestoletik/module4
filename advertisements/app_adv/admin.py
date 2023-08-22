b5978518eef541862b8b3fc3b595b4a1344d69f6
alinkaPestoletik <136910264+alinkaPestoletik@users.noreply.github.com>
2023-08-22 13:34:26 +0300

    Advertisements

---
+++ advertisements/app_adv/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from .models import Advertisements
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'description', 'price', 'created_date',
+                    'updated_date', 'auction', 'get_image']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+        ('Общее', {
+            'fields':('title', 'description', 'user', 'image')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+    @admin.display(description='изображение')
+    def get_image(self, obj):
+        from django.utils.html import format_html
+        if obj.image:
+            return format_html('<img src="{}" alt="Image" width="50" height="50" />', obj.image.url)
+        else:
+            return 'No Image'
+        display_thumbnail.short_description = 'Image'
+
+
+        # Также удалось вывести картинку при помощи функции mark_safe
+
+        # from django.utils.safestring import mark_safe
+        # if obj.image:
+        #     return mark_safe(f'<img src="{obj.image.url}" width=50>')
+        # else:
+        #     return 'No Image'
+
+admin.site.register(Advertisements, AdvertisementAdmin)
+
---
+++ advertisements/app_adv/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Advertisements
+from .forms import AdvertisementForm
+from django.urls import reverse
+
+def index(request):
+    advertisements = Advertisements.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisements(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')

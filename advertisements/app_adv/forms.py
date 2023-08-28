c3bc48388a55c8219484ff7fd8579ac667efb840
alinkaPestoletik <136910264+alinkaPestoletik@users.noreply.github.com>
2023-08-28 11:41:38 +0300

    Add files via upload

---
+++ advertisements/app_adv/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Advertisements
+
+class AdvertisementForm(forms.ModelForm):
+    class Meta:
+        model = Advertisements
+        fields = ["title", "description", "image", "price", "auction"]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
+            'description': forms.Textarea(attrs={'class': 'form-control form-control-lg'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control form-control-lg'}),
+            'auction': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'image': forms.FileInput(attrs={'class': 'form-control form-control-lg'})
+        }
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.startswith('?'):
+            raise ValidationError('Заголовок не может начинаться с ?')
+        return title
